6fb1a6e9d65d1781bde3a187551977d1d42df514
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-07 18:41:26 +0900

    Hit Judgment
    v.0.1.0 캐릭터 상단에 체력바, 피격 시 체력바 변동

---
+++ capstone/Assets/Scripts/HitJudgment.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitJudgment : MonoBehaviour
+{
+    public int HP = 100;
+    public GameObject HPbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Weapon")
+        {
+            HP -= 10;
+
+            RectTransform HPbar_rect = HPbar.GetComponent<RectTransform>();
+            HPbar_rect.offsetMin = new Vector2(0.0f, 235.0f);
+            HPbar_rect.offsetMax = new Vector2(-1000.0f + HP * 10.0f, -235.0f);
+        }
+    }
+}
