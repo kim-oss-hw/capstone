02e2fb50d549bc43bcd83bf179bbc5e2654fe8e4
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-08 02:06:53 +0900

    Hit Judgment
    v.0.2.0 피격 시 피격 애니메이션 추가

--- capstone/Assets/Scripts/HitJudgment.cs
+++ capstone/Assets/Scripts/HitJudgment.cs
@@ -6,6 +6,7 @@ public class HitJudgment : MonoBehaviour
 {
     public int HP = 100;
     public GameObject HPbar;
+    public GameObject AnimationObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,9 @@ public class HitJudgment : MonoBehaviour
             RectTransform HPbar_rect = HPbar.GetComponent<RectTransform>();
             HPbar_rect.offsetMin = new Vector2(0.0f, 235.0f);
             HPbar_rect.offsetMax = new Vector2(-1000.0f + HP * 10.0f, -235.0f);
+
+            Animator animator = AnimationObject.GetComponent<Animator>();
+            animator.SetTrigger("Hit");
         }
     }
 }
