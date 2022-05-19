1eededb106b8791190f80397a40a287fb347317c
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-20 05:30:32 +0900

    Final Fix

--- capstone/Assets/Scripts/PlayerSpawnManager.cs
+++ capstone/Assets/Scripts/PlayerSpawnManager.cs
@@ -17,11 +17,16 @@ public class PlayerSpawnManager : MonoBehaviourPunCallbacks
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings();
+        StartCoroutine(this.CreatePlayer());
+    }
 
+    IEnumerator CreatePlayer()
+    {
         if (NetworkManager.PlayerID % 2 == 1)
         {
             VRplayerPrefab = PhotonNetwork.Instantiate("Player", SpawnPoint1.transform.position, SpawnPoint1.transform.rotation);
             VRplayerPrefab.name = "Player1";
+
             OVRplayer = GameObject.Find("OVRPlayerController");
             OVRplayer.transform.position = SpawnPoint1.transform.position;
             OVRplayer.transform.rotation = SpawnPoint1.transform.rotation;
@@ -30,12 +35,17 @@ public class PlayerSpawnManager : MonoBehaviourPunCallbacks
         {
             VRplayerPrefab = PhotonNetwork.Instantiate("Player", SpawnPoint2.transform.position, SpawnPoint2.transform.rotation);
             VRplayerPrefab.name = "Player2";
+
             OVRplayer = GameObject.Find("OVRPlayerController");
             OVRplayer.transform.position = SpawnPoint2.transform.position;
             OVRplayer.transform.rotation = SpawnPoint2.transform.rotation;
         }
+        else
+        {
+            Instantiate(VRplayerPrefab, SpawnPoint2.transform.position, SpawnPoint2.transform.rotation);
+        }
+        yield return null;
     }
-
     public override void OnLeftRoom()
     {
         base.OnLeftRoom();
