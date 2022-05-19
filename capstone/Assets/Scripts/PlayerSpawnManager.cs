71b3d696f6f15d12e9712379a0774832f500aa96
HongJunSik <102528774+HongJunSik@users.noreply.github.com>
2022-05-20 01:45:41 +0900

    Revert "Test0519"
    This reverts commit 080f93f74958a4937fb582a4cfb30eeb587fc0a5.

--- capstone/Assets/Scripts/PlayerSpawnManager.cs
+++ capstone/Assets/Scripts/PlayerSpawnManager.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 
-public class SpawnManager : MonoBehaviourPunCallbacks
+public class PlayerSpawnManager : MonoBehaviourPunCallbacks
 {
     public GameObject _player1; // Player Prefab 설정
     public GameObject _player2; // Player Prefab 설정
@@ -13,27 +13,24 @@ public class SpawnManager : MonoBehaviourPunCallbacks
     public Transform _Spawn1; //플레이어1 스폰지역
     public Transform _Spawn2; //플레이어2 스폰지역
 
-    void Awake()
+    void Start()
     {
         StartCoroutine(this.CreatePlayer());
     }
 
-    void Update()
-    {
-
-    }
-
+    
     IEnumerator CreatePlayer()
     {
-        Debug.Log(NetworkManager.PlayerID);
-        if (NetworkManager.PlayerID % 2 == 1)
+        if (NetworkManager.PlayerID%2 == 1)
         {
             PhotonNetwork.Instantiate("Player", _Spawn1.position, _Spawn1.rotation);
         }
-        else if (NetworkManager.PlayerID % 2 == 0)
+        else if (NetworkManager.PlayerID%2 == 0)
         {
             PhotonNetwork.Instantiate("Player", _Spawn2.position, _Spawn2.rotation);
         }
+        else
+            PhotonNetwork.Instantiate("Player", _Spawn1.position, _Spawn1.rotation);
         yield return null;
 
     }
