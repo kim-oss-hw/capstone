a74f0461564ddc0d5024ff1b1db0d5fdcbf95c96
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-20 03:40:14 +0900

    Error Fix

--- capstone/Assets/Scripts/PlayerSpawnManager.cs
+++ capstone/Assets/Scripts/PlayerSpawnManager.cs
@@ -13,19 +13,11 @@ public class PlayerSpawnManager : MonoBehaviourPunCallbacks
     public Transform SpawnPoint2; //플레이어2 스폰지역
 
 
+
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings();
-    }
 
-    public override void OnConnectedToMaster()
-    {
-        base.OnConnectedToMaster();
-        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions { MaxPlayers = 2 }, null);
-    }
-
-    public override void OnJoinedRoom()
-    {
         base.OnJoinedRoom();
 
         if (NetworkManager.PlayerID % 2 == 1)
