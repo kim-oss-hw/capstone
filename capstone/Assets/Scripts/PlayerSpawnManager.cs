769fb872a4e3c20c76b4eb2e1407f2ee26003e4e
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-20 00:17:52 +0900

    error test

--- capstone/Assets/Scripts/PlayerSpawnManager.cs
+++ capstone/Assets/Scripts/PlayerSpawnManager.cs
@@ -7,7 +7,9 @@ using UnityEngine.UI;
 
 public class PlayerSpawnManager : MonoBehaviourPunCallbacks
 {
-    public GameObject _player; // Player Prefab 설정
+    public GameObject _player1; // Player Prefab 설정
+    public GameObject _player2; // Player Prefab 설정
+
     public Transform _Spawn1; //플레이어1 스폰지역
     public Transform _Spawn2; //플레이어2 스폰지역
 
@@ -21,14 +23,14 @@ public class PlayerSpawnManager : MonoBehaviourPunCallbacks
     {
         if (NetworkManager.PlayerID%2 == 1)
         {
-            PhotonNetwork.Instantiate("Player", _Spawn1.position, _Spawn1.rotation);
+            PhotonNetwork.Instantiate("Player1", _Spawn1.position, _Spawn1.rotation);
         }
         else if (NetworkManager.PlayerID%2 == 0)
         {
-            PhotonNetwork.Instantiate("Player", _Spawn2.position, _Spawn2.rotation);
+            PhotonNetwork.Instantiate("Player2", _Spawn2.position, _Spawn2.rotation);
         }
         else
-            PhotonNetwork.Instantiate("Player", _Spawn1.position, _Spawn1.rotation);
+            PhotonNetwork.Instantiate("Player1", _Spawn1.position, _Spawn1.rotation);
         yield return null;
 
     }
