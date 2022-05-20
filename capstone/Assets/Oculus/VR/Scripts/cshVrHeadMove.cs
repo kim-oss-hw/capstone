e9c36271343c9836c821f7880ce39d4c9d009533
Yumcow <79wogud@naver.com>
2022-05-21 00:40:05 +0900

    Prefab

--- capstone/Assets/Oculus/VR/Scripts/cshVrHeadMove.cs
+++ capstone/Assets/Oculus/VR/Scripts/cshVrHeadMove.cs
@@ -1,3 +1,4 @@
+///*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,19 +8,25 @@ public class cshVrHeadMove : MonoBehaviour
     public Camera camera;
     public float speedForward = 1;
     public float speedSide = 1;
+    public float speedRota = 1;
+    public float equipDistance = 6;
     public GameObject rightHand;
     public GameObject leftHand;
-    public GameObject swordOneHand;
+    //public GameObject swordOneHand;
+    public GameObject[] swords = new GameObject[3];
+    public GameObject playerCharacter;
 
-    private bool grabbing = false;
     private Transform tr;
-    private float dirX = 0;
-    private float dirZ = 0;
+    //private float dirX = 0;
+    //private float dirZ = 0;
+    private GameObject curSword = null;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         tr = GetComponent<Transform>();
+        animator = playerCharacter.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,66 +34,55 @@ public class cshVrHeadMove : MonoBehaviour
     {
         RaycastHit hit;
 
+        if (!playerCharacter)
+        {
+
+        }
         MovePlayer();
         if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
         {
-            Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 5);
-
-            if (grabbing)
+            if (curSword)
             {
-                Transform sword = rightHand.transform.Find("1HandSword(Clone)");
-                if (sword)
-                {
-                    Destroy(sword.gameObject);
-                    grabbing = false;
-                } else
-                {
-                    sword = leftHand.transform.Find("1HandSword(Clone)");
-                    Destroy(sword.gameObject);
-                    grabbing = false;
-                }
+                unequipSword();
             }
             else
             {
-                print(hit.transform.parent.name);
-                if (hit.transform.parent.tag == "Weapon")
+                Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, equipDistance);
+                if (hit.transform.tag == "Broadsword")
                 {
-                    GameObject.Instantiate(swordOneHand, rightHand.transform.position, rightHand.transform.rotation).transform.parent = rightHand.transform;
-                    GameObject sword = rightHand.transform.Find("1HandSword(Clone)").gameObject;
-                    sword.transform.localPosition = new Vector3(0.102f, -0.052f, -0.01f);
-                    sword.transform.localRotation = Quaternion.Euler(49.934f, -61.109f, -85.92f);
-                    grabbing = true;
+                    equipSword(0, true);
+                }
+                else if (hit.transform.tag == "Shortsword")
+                {
+                    equipSword(1, true);
+                }
+                else if (hit.transform.tag == "Falchion")
+                {
+                    equipSword(2, true);
                 }
             }
-        } else if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
         {
-            Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 3);
-
-            if (grabbing)
+            if (curSword)
             {
-                Transform sword = leftHand.transform.Find("1HandSword(Clone)");
-                if (sword)
-                {
-                    Destroy(sword.gameObject);
-                    grabbing = false;
-                } else
-                {
-                    sword = rightHand.transform.Find("1HandSword(Clone)");
-                    Destroy(sword.gameObject);
-                    grabbing = false;
-                }
-                
+                unequipSword();
             }
             else
             {
-                print(hit.transform.parent.name);
-                if (hit.transform.parent.tag == "Weapon")
+                Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, equipDistance);
+                print(hit.transform.name);
+                if (hit.transform.tag == "Broadsword")
                 {
-                    GameObject.Instantiate(swordOneHand, leftHand.transform.position, leftHand.transform.rotation).transform.parent = leftHand.transform;
-                    GameObject sword = leftHand.transform.Find("1HandSword(Clone)").gameObject;
-                    sword.transform.localPosition = new Vector3(-0.109f, 0.051f, -0.01f);
-                    sword.transform.localRotation = Quaternion.Euler(-83.931f, 0f, 23.141f);
-                    grabbing = true;
+                    equipSword(0, false);
+                }
+                else if (hit.transform.tag == "Shortsword")
+                {
+                    equipSword(1, false);
+                }
+                else if (hit.transform.tag == "Falchion")
+                {
+                    equipSword(2, false);
                 }
             }
         }
@@ -94,9 +90,84 @@ public class cshVrHeadMove : MonoBehaviour
 
     void MovePlayer()
     {
-        Vector2 coord = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
-        Vector3 moveDir = new Vector3(coord.x * speedSide, 0, coord.y * speedForward);
+        Vector2 coordMove = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Vector2 coordRota = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        Vector3 moveDir = new Vector3(coordMove.x * speedSide, 0, coordMove.y * speedForward);
+        Vector3 rotaDir = new Vector3(0f, coordRota.x * speedRota, 0f);
 
         transform.Translate(moveDir * Time.deltaTime);
+        transform.Rotate(rotaDir * Time.deltaTime);
+    }
+
+    void equipSword(int swordIndex, bool isRight)
+    {
+        if (curSword)
+        {
+            unequipSword();
+            return;
+        }
+        switch (swordIndex)
+        {
+            case 0: // Broadsword
+                if (isRight)
+                {
+                    curSword = GameObject.Instantiate(swords[swordIndex], rightHand.transform.position, rightHand.transform.rotation);
+                    curSword.transform.parent = rightHand.transform;
+                    curSword.transform.localPosition = new Vector3(0.075f, -0.045f, -0.04f);
+                    curSword.transform.localRotation = Quaternion.Euler(164.767f, -61.332f, 3.149002f);
+                }
+                else
+                {
+                    curSword = GameObject.Instantiate(swords[swordIndex], leftHand.transform.position, leftHand.transform.rotation);
+                    curSword.transform.parent = leftHand.transform;
+                    curSword.transform.localPosition = new Vector3(-0.0739f, 0.035f, 0.0282f);
+                    curSword.transform.localRotation = Quaternion.Euler(-165.083f, 117.143f, -5.265015f);
+                }
+                break;
+            case 1: // Shortsword
+                if (isRight)
+                {
+                    curSword = GameObject.Instantiate(swords[swordIndex], rightHand.transform.position, rightHand.transform.rotation);
+                    curSword.transform.parent = rightHand.transform;
+                    curSword.transform.localPosition = new Vector3(0.09f, -0.048f, 0.008f);
+                    curSword.transform.localRotation = Quaternion.Euler(164.767f, -61.332f, 3.149002f);
+                }
+                else
+                {
+                    curSword = GameObject.Instantiate(swords[swordIndex], leftHand.transform.position, leftHand.transform.rotation);
+                    curSword.transform.parent = leftHand.transform;
+                    curSword.transform.localPosition = new Vector3(-0.0924f, 0.0565f, 0.0023f);
+                    curSword.transform.localRotation = Quaternion.Euler(-165.083f, 117.143f, -5.265015f);
+                }
+                break;
+            case 2: // Falchion
+                if (isRight)
+                {
+                    curSword = GameObject.Instantiate(swords[swordIndex], rightHand.transform.position, rightHand.transform.rotation);
+                    curSword.transform.parent = rightHand.transform;
+                    curSword.transform.localPosition = new Vector3(0.0948f, -0.0442f, 0.0044f);
+                    curSword.transform.localRotation = Quaternion.Euler(164.767f, -61.332f, 3.149002f);
+                }
+                else
+                {
+                    curSword = GameObject.Instantiate(swords[swordIndex], leftHand.transform.position, leftHand.transform.rotation);
+                    curSword.transform.parent = leftHand.transform;
+                    curSword.transform.localPosition = new Vector3(-0.094f, 0.038f, -0.002f);
+                    curSword.transform.localRotation = Quaternion.Euler(-165.083f, 117.143f, -5.265015f);
+                }
+                break;
+            default: return;
+        }
+        if (isRight) animator.SetBool("rightGrabing", true);
+        else animator.SetBool("leftGrabing", true);
+    }
+
+    void unequipSword()
+    {
+        if (!curSword) return;
+        animator.SetBool("rightGrabing", false);
+        animator.SetBool("leftGrabing", false);
+        Destroy(curSword);
     }
 }
+//*/
