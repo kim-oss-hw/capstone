75e9b95462980fa3f07e701b33af61847ead4d53
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-21 23:44:12 +0900

    0521 Update

--- capstone/Assets/Scripts/HitJudgment.cs
+++ capstone/Assets/Scripts/HitJudgment.cs
@@ -8,7 +8,8 @@ using UnityEngine.UI;
 public class HitJudgment : MonoBehaviourPunCallbacks
 {
     public float HP = 100.0f;
-    public GameObject HPbar;
+    public GameObject HPbar_top;
+    public GameObject HPbar_ui;
     public GameObject AnimationObject;
     WeaponSystem WeaponSystem;
 
@@ -35,9 +36,14 @@ public class HitJudgment : MonoBehaviourPunCallbacks
                 EnemyWeapon.Attackable = false;
                 EnemyWeapon.CoolTimeStart = true;
 
-                RectTransform HPbar_rect = HPbar.GetComponent<RectTransform>();
+                RectTransform HPbar_rect = HPbar_top.GetComponent<RectTransform>();
+                RectTransform HPbarUI_rect = HPbar_ui.GetComponent<RectTransform>();
+
                 HPbar_rect.offsetMin = new Vector2(0.0f, 235.0f);
                 HPbar_rect.offsetMax = new Vector2(-1000.0f + HP * 10.0f, -235.0f);
+
+                HPbarUI_rect.offsetMin = new Vector2(-237.0f, 17.0f - HP * 10.0f);
+                HPbarUI_rect.offsetMax = new Vector2(1363.0f, -383.0f);
 
                 //피격자 경직 애니메이션 부분
                 Animator animator = AnimationObject.GetComponent<Animator>();
--- capstone/Assets/Scripts/WeaponSystem.cs
+++ capstone/Assets/Scripts/WeaponSystem.cs
@@ -25,7 +25,6 @@ public class WeaponSystem : MonoBehaviour
             castbarXY.offsetMin = new Vector2(-137.0f, 267.0f + i * 1.0f);
             castbarXY.offsetMax = new Vector2(1263.0f, 267.0f);
 
-
             //castbarXY.sizeDelta = new Vector2(i * 10.0f, f);
             yield return new WaitForSeconds(CoolTime / 100.0f);
         }
