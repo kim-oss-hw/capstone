a5672af03b504e5cb53723e226f2cdd1a326a2cf
Yumcow <79wogud@naver.com>
2022-05-24 07:09:34 +0900

    weapon fix 1

---
+++ capstone/Assets/Scripts/cshSettingWeapon.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class cshSettingWeapon : MonoBehaviour
+{
+    public bool[,] weapons = new bool[2, 3] { { false, false, false }, { false, false, false } };
+    public GameObject rightHand;
+    public GameObject leftHand;
+    public GameObject curSword;
+    public Animator animator;
+    public PhotonView PV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightHand = transform.Find("PlayerCharacter/root/pelvis/spine_01/spine_02/spine_03/clavicle_r/upperarm_r/lowerarm_r/hand_r").gameObject;
+        leftHand = transform.Find("PlayerCharacter/root/pelvis/spine_01/spine_02/spine_03/clavicle_l/upperarm_l/lowerarm_l/hand_l").gameObject;
+        animator = transform.Find("PlayerCharacter").gameObject.GetComponent<Animator>();
+        PV = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void unequipSword()
+    {
+        if (!curSword) return;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (weapons[i, j])
+                {
+                    if (i == 0) PV.RPC("DeactivateWeapon", RpcTarget.All, j, true);
+                    else PV.RPC("DeactivateWeapon", RpcTarget.All, j, false);
+                    weapons[i, j] = false;
+                }
+            }
+        }
+        curSword = null;
+    }
+
+    public void equipSword(int swordIndex, bool isRight)
+    {
+        if (swordIndex > 2)
+        {
+            unequipSword();
+            return;
+        }
+
+        if (isRight)
+        {
+            curSword = rightHand.transform.GetChild(swordIndex + 5).gameObject;
+            //curSword.SetActive(true);
+            PV.RPC("ActivateWeapon", RpcTarget.All, swordIndex, true);
+            weapons[0, swordIndex] = true;
+            //animator.SetBool("rightGrabing", true);
+        }
+        else
+        {
+            curSword = leftHand.transform.GetChild(swordIndex + 5).gameObject;
+            //curSword.SetActive(true);
+            PV.RPC("ActivateWeapon", RpcTarget.All, swordIndex, false);
+            weapons[1, swordIndex] = true;
+            //animator.SetBool("leftGrabing", true);
+        }
+    }
+
+    public bool isEquiped()
+    {
+        if (curSword) return true;
+        else return false;
+    }
+
+    [PunRPC]
+    void ActiveWeapon(int weaponIndex, bool isRight)
+    {
+        if (isRight)
+        {
+            rightHand.transform.GetChild(weaponIndex + 5).gameObject.SetActive(true);
+            animator.SetBool("rightGrabing", true);
+        }
+        else
+        {
+            leftHand.transform.GetChild(weaponIndex + 5).gameObject.SetActive(true);
+            animator.SetBool("leftGrabing", true);
+        }
+    }
+
+    [PunRPC]
+    void DeactiveWeapon(int weaponIndex, bool isRight)
+    {
+        if (isRight)
+        {
+            rightHand.transform.GetChild(weaponIndex).gameObject.SetActive(false);
+        }
+        else
+        {
+            leftHand.transform.GetChild(weaponIndex).gameObject.SetActive(false);
+        }
+        animator.SetBool("rightGrabing", false);
+        animator.SetBool("leftGrabing", false);
+    }
+}
--- capstone/Assets/Scripts/cshVrHeadMove.cs
+++ capstone/Assets/Scripts/cshVrHeadMove.cs
@@ -1,43 +1,138 @@
+///*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class cshVrHeadMove : MonoBehaviour
 {
+    public Camera camera;
     public float speedForward = 1;
     public float speedSide = 1;
+    public float speedRota = 1;
+    public float equipDistance = 6;
 
     public float RimitX_po = 1;
     public float RimitX_ne = 1;
+    public float RimitY_po = 1;
+    public float RimitY_ne = 1;
     public float RimitZ_po = 1;
     public float RimitZ_ne = 1;
 
+    private cshSettingWeapon settingWeapon;
+    private GameObject player;
     private Transform tr;
-    private float dirX = 0;
-    private float dirZ = 0;
+    private bool isSetting = true;
 
     // Start is called before the first frame update
     void Start()
     {
         tr = GetComponent<Transform>();
+        camera = transform.Find("TrackingSpace/CenterEyeAnchor").gameObject.GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+
+        if (!player) // player = null ¿œ∂ß
+        {
+            if (GameObject.Find("MyPlayer") || GameObject.Find("Player"))
+            {
+                if (GameObject.Find("MyPlayer")) player = GameObject.Find("MyPlayer");
+                else player = GameObject.Find("Player");
+                settingWeapon = player.GetComponent<cshSettingWeapon>();
+                //rightHand = player.transform.
+                //Find("PlayerCharacter/root/pelvis/spine_01/spine_02/spine_03/clavicle_r/upperarm_r/lowerarm_r/hand_r").gameObject;
+                //leftHand = player.transform.
+                //Find("PlayerCharacter/root/pelvis/spine_01/spine_02/spine_03/clavicle_l/upperarm_l/lowerarm_l/hand_l").gameObject;
+                //animator = player.transform.Find("PlayerCharacter").gameObject.GetComponent<Animator>();
+            }
+        }
+
         MovePlayer();
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            if (isSetting)
+            {
+                if (!Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, equipDistance))
+                {
+                    if (settingWeapon.isEquiped())
+                    {
+                        settingWeapon.equipSword(3, true);
+                    }
+                }
+                else
+                {
+                    if (hit.transform.tag == "SetWeapon" && settingWeapon.isEquiped())
+                    {
+                        isSetting = false;
+                        Destroy(hit.transform.parent.gameObject);
+                    }
+                    else if (hit.transform.tag == "Broadsword")
+                    {
+                        settingWeapon.equipSword(0, true);
+                    }
+                    else if (hit.transform.tag == "Shortsword")
+                    {
+                        settingWeapon.equipSword(1, true);
+                    }
+                    else if (hit.transform.tag == "Falchion")
+                    {
+                        settingWeapon.equipSword(2, true);
+                    }
+                }
+            }
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        {
+            if (isSetting)
+            {
+                if (!Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, equipDistance))
+                {
+                    if (settingWeapon.isEquiped())
+                    {
+                        settingWeapon.equipSword(3, false);
+                    }
+                }
+                else
+                {
+                    if (hit.transform.tag == "SetWeapon" && settingWeapon.isEquiped())
+                    {
+                        isSetting = false;
+                        Destroy(hit.transform.parent.gameObject);
+                    }
+                    else if (hit.transform.tag == "Broadsword")
+                    {
+                        settingWeapon.equipSword(0, false);
+                    }
+                    else if (hit.transform.tag == "Shortsword")
+                    {
+                        settingWeapon.equipSword(1, false);
+                    }
+                    else if (hit.transform.tag == "Falchion")
+                    {
+                        settingWeapon.equipSword(2, false);
+                    }
+                }
+            }
+        }
     }
 
     void MovePlayer()
     {
-        Vector2 coord = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
-        Vector3 moveDir = new Vector3(coord.x * speedSide, 0, coord.y * speedForward);
+        Vector2 coordMove = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Vector2 coordRota = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        Vector3 moveDir = new Vector3(coordMove.x * speedSide, 0, coordMove.y * speedForward);
+        Vector3 rotaDir = new Vector3(0f, coordRota.x * speedRota, 0f);
 
-        transform.Translate(moveDir * Time.deltaTime);
+        if (!isSetting)
+        {
+            transform.Translate(moveDir * Time.deltaTime);
+        }
 
-        transform.Translate(new Vector3(Mathf.Clamp(transform.position.x, RimitX_ne, RimitX_po), transform.position.y, Mathf.Clamp(transform.position.z, RimitZ_po, RimitZ_po)));
-
-        //Rigidbody PlayerRigidbody = gameObject.GetComponent<Rigidbody>();
-        //PlayerRigidbody.velocity = Vector3.zero;
+        transform.Rotate(rotaDir * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, RimitX_ne, RimitX_po), Mathf.Clamp(transform.position.y, RimitY_ne, RimitY_po), Mathf.Clamp(transform.position.z, RimitZ_ne, RimitZ_po));
     }
 }
+//*/
