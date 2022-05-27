d40781e3e1ccfa8921df3290aee9b5fd1cbfb1f2
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-28 00:51:25 +0900

    Game System Fix

--- capstone/Assets/Scripts/WeaponSystem.cs
+++ capstone/Assets/Scripts/WeaponSystem.cs
@@ -40,16 +40,16 @@ public class WeaponSystem : MonoBehaviour, IPunObservable
 
         int i = 0;
 
-        while (i < 100)
+        while (i < 10)
         {
             i += 1;
             float f = i * 1.0f;
 
-            castbarXY.offsetMin = new Vector2(637.0f, 1618.0f + 500.0f - (i * 5.0f));
-            castbarXY.offsetMax = new Vector2(2037.0f, 618.0f);
+            castbarXY.offsetMin = new Vector2(637.0f, 1618.0f + 500.0f);
+            castbarXY.offsetMax = new Vector2(-2037.0f, -618.0f + 500.0f - (i * 50.0f));
 
             //castbarXY.sizeDelta = new Vector2(i * 10.0f, f);
-            yield return new WaitForSeconds(CoolTime / 100.0f);
+            yield return new WaitForSeconds(CoolTime / 10.0f);
         }
 
         Attackable = true;
