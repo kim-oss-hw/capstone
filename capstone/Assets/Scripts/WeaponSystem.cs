84470ee58e26a9aa2edfe8e010fbd770917082b5
kim-capstone <75201939+kim-oss-hw@users.noreply.github.com>
2022-05-29 00:54:11 +0900

    test

--- capstone/Assets/Scripts/WeaponSystem.cs
+++ capstone/Assets/Scripts/WeaponSystem.cs
@@ -45,8 +45,8 @@ public class WeaponSystem : MonoBehaviour, IPunObservable
             i += 1;
             float f = i * 1.0f;
 
-            castbarXY.offsetMin = new Vector2(637.0f, 618.0f);
-            castbarXY.offsetMax = new Vector2(-2037.0f, -1618.0f - 500.0f + (i * 25.0f));
+            castbarXY.offsetMin = new Vector2(637.0f, 668.0f);
+            castbarXY.offsetMax = new Vector2(-2037.0f, -1668.0f - 500.0f + (i * 25.0f));
 
             //castbarXY.sizeDelta = new Vector2(i * 10.0f, f);
             yield return new WaitForSeconds(CoolTime / 20.0f);
